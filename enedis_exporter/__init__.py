daf0aa479c5ef32aa52765bad2250587e6f2e3f0
Léo Colombaro <git@colombaro.fr>
2023-03-01 16:48:54 +0000

    Prepare enedis

--- enedis_exporter/__init__.py
+++ enedis_exporter/__init__.py
@@ -9,8 +9,8 @@ from sentry_sdk import capture_exception
 from influxdb_client import Point
 import influxdb_exporter
 
-from sdk_enedis.api import API
-enedis = API(
+import enedis_exporter.enedis
+enedis = enedis_exporter.enedis.API(
     os.environ.get("ENEDIS_CLIENT_ID"),
     os.environ.get("ENEDIS_CLIENT_SECRET")
 )
@@ -24,25 +24,30 @@ def fetch():
     try:
         for year in range(3):
             start = today.replace(year=today.year - year)
-            for releve in enedis.daily_consumption(
+            print(enedis.daily_consumption(
                 os.environ.get("PDL"),
                 from_date=(start - delta).isoformat(),
                 to_date=(start).isoformat()
-            ):
-                conso = releve["consommation"]
-                points.append(Point("enedis")
-                    .time(datetime.fromisoformat(conso["date_fin_consommation"]).replace(year=today.year))
-                    .tag("year", start.year)
-                    .field("energy", conso["energie"])
-                )
+            ))
+            # for releve in enedis.daily_consumption(
+            #     os.environ.get("PDL"),
+            #     from_date=(start - delta).isoformat(),
+            #     to_date=(start).isoformat()
+            # ):
+            #     conso = releve["consommation"]
+            #     points.append(Point("enedis")
+            #         .time(datetime.fromisoformat(conso["date_fin_consommation"]).replace(year=today.year))
+            #         .tag("year", start.year)
+            #         .field("energy", conso["energie"])
+            #     )
 
     except Exception as e:
         capture_exception(e)
 
     return points
 
-@repeat(every(12).hours)
+@repeat(every(1).seconds)
 def enedis_exporter():
     points = fetch()
-    for point in points:
-        influxdb_exporter.InfluxDB().push(point)
+    # for point in points:
+    #     influxdb_exporter.InfluxDB().push(point)
