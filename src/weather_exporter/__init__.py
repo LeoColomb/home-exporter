a65802aefce07230e33141358356afa8b784cfc5
Léo Colombaro <LeoColomb@users.noreply.github.com>
2025-01-04 20:02:10 +0100

    Weather exporter v2

--- src/weather_exporter/__init__.py
+++ src/weather_exporter/__init__.py
@@ -10,36 +10,23 @@ import influxdb_exporter
 
 import requests
 
-def hdd(Tn: float, Tx: float, Tmoy: float, Tref: float=17.0) -> float:
-    r"""Heating Degree Days
-
-    :param Tn: Minimum day temperature.
-    :param Tx: Maximum day temperature.
-    :param Tref: Reference temperature.
-    """
-    if Tref > Tx:
-        return Tref - (Tmoy if Tmoy else ((Tn + Tx) / 2))
-    elif Tref <= Tn:
-        return 0.0
-    else:
-        return (Tref - Tn) * (0.08 + 0.42 * (Tref - Tn) / (Tx - Tn))
-
 def reqData(
     location: str,
     latitude: float,
     longitude: float,
-    start: date,
     today: date,
-    delta: timedelta
+    dayDelta: timedelta,
+    yearDelta: int=0
 ) -> Point:
     r"""Request data handler
     """
     points = []
+    start = today - timedelta(days=365 * yearDelta)
 
     r = requests.get('https://archive-api.open-meteo.com/v1/archive', {
         'latitude': latitude,
         'longitude': longitude,
-        'start_date': start - delta,
+        'start_date': start - dayDelta,
         'end_date': start,
         'models': 'best_match',
         'daily': [
@@ -53,9 +40,9 @@ def reqData(
     for i in range(len(result['daily']['time'])):
         if not result['daily']['temperature_2m_mean'][i]:
             continue
-        points.append(Point('weather')
+        points.append(Point('weather_v2')
             .time(datetime.fromisoformat(result['daily']['time'][i]).replace(year=today.year))
-            .tag('year', start.year)
+            .tag('year', -yearDelta)
             .tag('location', location)
             .field('temperature_min', result['daily']['temperature_2m_min'][i])
             .field('temperature_max', result['daily']['temperature_2m_max'][i])
@@ -63,11 +50,6 @@ def reqData(
                 'temperature_mean',
                 result['daily']['temperature_2m_mean'][i]
             )
-            .field('degree_day', hdd(
-                result['daily']['temperature_2m_min'][i],
-                result['daily']['temperature_2m_max'][i],
-                result['daily']['temperature_2m_mean'][i],
-            )))
     return points
 
 
@@ -83,17 +65,16 @@ def fetch() -> Point:
                 location='Paris',
                 latitude=48.83,
                 longitude=2.35,
-                start=today.replace(year=today.year - year),
                 today=today,
-                delta=delta
+                dayDelta=delta,
+                yearDelta=year
             ))
         points.extend(reqData(
             location='Copenhagen',
             latitude=55.68,
             longitude=12.57,
-            start=today,
             today=today,
-            delta=delta
+            dayDelta=delta
         ))
     except Exception as e:
         capture_exception(e)
